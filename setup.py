f4e0d292c192c6b0084acd570c412a6c260c6d74
Jan Willhaus <mail@janwillhaus.de>
2017-01-05 11:32:30 +0100

    Added distutils setup, now pip compatible

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='PyPrimion',
+      version='0.1',
+      description='Webinterface handler for the Primion Prime Web Systems time tracking',
+      author='Jan Willhaus',
+      author_email='mail@janwillhaus.de',
+      license='MIT (X11) License',
+      py_modules=['pyprimion'],
+      url='https://github.com/Janwillhaus/PyPrimion',
+      packages=None,
+      long_description=open('README.md').read(),
+      )
